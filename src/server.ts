facb8bcfabb7ea20b48f0ee5eed7a17b6af6ddf8
Henrique M <henriquemanduca@live.com>
2020-07-19 13:57:00 -0300

    helmet

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 
 import mongoose from './database';
 import routes from './routes';
@@ -6,6 +7,7 @@ import routes from './routes';
 const PORT = Number(process.env.PORT) || 3006;
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 
 app.on('ready', () => {
